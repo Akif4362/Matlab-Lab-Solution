39b0f00e4858c63d33e90a8c3605676a8ca0436e
Akif4362 <153272461+Akif4362@users.noreply.github.com>
2026-05-19 15:51:17 +0600

    Update q1.m

--- Assignment-4/q1.m
+++ Assignment-4/q1.m
@@ -12,7 +12,7 @@ title("(a) log scale")
 % order 2
 x = log10(T);
 y = log10(k);
-p2 = polyfit(x, y, 2); % dont use polyfit use curve fitting algorithm. i will update this code later
+p2 = curvefit2(x, y); % dont use polyfit use curve fitting algorithm. i will update this code later
 
 syms k_syms T_syms
 k_eqn = solve(log10(k_syms) == p2(1)*log10(T_syms)^2 + p2(2)*log10(T_syms) + p2(3),...
@@ -29,7 +29,7 @@ plot(T_span, kval(T_span))
 % order 3
 x = log10(T);
 y = log10(k);
-p2 = polyfit(x, y, 3);
+p2 = curvefit3(x, y);
 
 syms k_syms T_syms
 k_eqn = solve(log10(k_syms) == p2(1)*log10(T_syms)^3 + p2(2)*log10(T_syms)^2 + p2(3)*log10(T_syms) + p2(4),...
@@ -43,3 +43,33 @@ p2_span = polyval(p2, T_span);
 plot(T_span, kval(T_span))
 
 legend("real data", "order 2", "order 3")
+
+% functions
+function coef = curvefit2(xpoint, ypoint)
+    nval = length(xpoint);
+    sumx = sum(xpoint);
+    sumx2 = sum(xpoint.^2);
+    sumx3 = sum(xpoint.^3);
+    sumx4 = sum(xpoint.^4);
+    sumy = sum(ypoint);
+    sumxy = sum(xpoint.*ypoint);
+    sumx2y = sum((xpoint.^2).*ypoint);
+    coef = [nval sumx sumx2; sumx sumx2 sumx3; sumx2 sumx3 sumx4] \ [sumy; sumxy; sumx2y];
+    coef = flip(coef');
+end
+
+function coef = curvefit3(xpoint, ypoint)
+    nval = length(xpoint);
+    sumx = sum(xpoint);
+    sumx2 = sum(xpoint.^2);
+    sumx3 = sum(xpoint.^3);
+    sumx4 = sum(xpoint.^4);
+    sumx5 = sum(xpoint.^5);
+    sumx6 = sum(xpoint.^6);
+    sumy = sum(ypoint);
+    sumxy = sum(xpoint.*ypoint);
+    sumx2y = sum((xpoint.^2).*ypoint);
+    sumx3y = sum((xpoint.^3).*ypoint);
+    coef = [nval sumx sumx2 sumx3; sumx sumx2 sumx3 sumx4; sumx2 sumx3 sumx4 sumx5; sumx3 sumx4 sumx5 sumx6] \ [sumy; sumxy; sumx2y; sumx3y];
+    coef = flip(coef');
+end
